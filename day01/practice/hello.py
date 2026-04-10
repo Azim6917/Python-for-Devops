99f08433db9a7260981dcd94c55a1c27f110bfa2
Azim6917 <sarwadazim786@gmail.com>
2026-04-10 20:33:53 +0530

    Basics of python

---
+++ day01/practice/hello.py
@@ -0,0 +1,38 @@
+'''
+Scripts is a set of instructions.
+variables & constants.
+ ---------------------------------------------------------------
+what is variable ?
+ A **variable in Python** is a named container used to store data that can be changed during program execution.
+ ------------------------------------------------------------------------------------------------------------------
+what is constant?
+A **constant in Python** is a value that is meant to stay the same and not change while the program runs.
+ ------------------------------------------------------------------------------------------------------------------------
+what is assignment operator?
+An **assignment operator in Python** is used to give a value to a variable (like `=`).
+-----------------------------------------------------------------------------------------------------------------------
+what is arithmetic operators?
+In Python, **arithmetic operators** are symbols used to perform basic math operations like addition, subtraction, multiplication, and division.
+----------------------------------------------------------------------------------------------------------------------------------------------------
+what are Operators and Operands?
+Operands are the values you use (like numbers or variables)
+Operators (operations) are the symbols that perform actions on them
+For example - a + b
+
+a and b = operands
++ = operator (operation)
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+
+print("Hello world")
+
+a = 1000
+b = 1000
+ans = a+b
+print("sum of a and b is ", ans)
+
+x = 100204
+y = 260903
+z = x*y
+print("multiplication of x and y is ", z)
