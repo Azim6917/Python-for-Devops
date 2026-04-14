6f5a21cf398c916d24c7a929650a1450c8547b2f
Azim6917 <sarwadazim786@gmail.com>
2026-04-14 13:21:32 +0530

    collection.py created

---
+++ practice/collection.py
@@ -0,0 +1,14 @@
+list_of_animals = ["lion", "tiger", "bear", "elephant", "monkey", "muskan"]
+
+print(list_of_animals)
+
+list_of_animals.append("donkey")
+list_of_animals.append("Giraffe")
+
+print(list_of_animals)
+
+list_of_animals.insert(3, "Cheetah")
+
+print(list_of_animals)
+
+print(len(list_of_animals))
