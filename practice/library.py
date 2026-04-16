2b6b911770d8ba249c5435097bcbf56475466d97
Azim6917 <sarwadazim786@gmail.com>
2026-04-16 13:17:05 +0530

    added library.py

---
+++ practice/library.py
@@ -0,0 +1,4 @@
+import os
+
+print (os.system("ping google.com"))
+print (os.system("uname -a"))
--- practice/loops.py
+++ practice/loops.py
@@ -2,3 +2,7 @@ list_of_tree = ["banyan", "neem", "peepal", "mango", "jamun"]
 
 for tree in list_of_tree:
     print(tree)
+
+
+for i in range (1,11):
+    print(i)
