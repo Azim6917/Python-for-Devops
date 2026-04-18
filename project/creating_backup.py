00bb86e940b72c9d4618219e5cb402adc18a6e87
Azim6917 <sarwadazim786@gmail.com>
2026-04-18 15:56:29 +0000

    added AWS S3 Backup Automation using Python

--- project/creating_backup.py
+++ project/creating_backup.py
@@ -4,8 +4,8 @@ import shutil
 
 def backup (source, destination):
     today = datetime.date.today()
-    backup_file_name = os.path.join(destination,f"backup{today}.tar.gz" )
-    shutil.make_archive(backup_file_name.replace(".tar.gz", " "),'gztar', source)
+    backup_file_name = os.path.join(destination,f"backup{today}" )
+    shutil.make_archive(backup_file_name,'gztar', source)
 
 source =  "/mnt/e/DevOps/Python/project"
 destination = "/mnt/e/DevOps/Python/project/backup"  
--- project/s3_backup.py
+++ project/s3_backup.py
@@ -5,20 +5,55 @@ boto3 -> used to do AWS tasks using python.
 """
 
 import boto3
+import os
+import datetime
+import shutil
 
 s3 = boto3.resource("s3")
 def show_bucket(s3):
+    print("*** List of current buckets in s3 ***")
     for buckets in s3.buckets.all():
         print(buckets.name)
 
 def create_bucket(s3):
-    s3.create_bucket(Bucket="python769245602", 
-                    CreateBucketConfiguration={
-                    'LocationConstraint': 'us-east-2',
-                    },)
+        s3.create_bucket(Bucket="pythonfordevops2609", 
+                        CreateBucketConfiguration={
+                        'LocationConstraint': 'us-east-2',
+                        },)
+
+        print("Bucket has been created successfully.....!!!")
+        print("...")
+
+source =  "/mnt/e/DevOps/Python/project"
+destination = "/mnt/e/DevOps/Python/project/backup" 
+
+today = datetime.date.today()
+backup_file_name = os.path.join(destination,f"backup{today}.tar.gz" )
+
+
+def backup(source, destination):
+    shutil.make_archive(backup_file_name.replace(".tar.gz", " "),'gztar', source) 
+    print("Backup has been created successfully.....!!!")
+    print("...")
+
+
+def upload_backup(s3, backup_file_name, bucket_name, key_name):
+    data = open(backup_file_name, 'rb') # this will read files in binary
+    s3.Bucket(bucket_name).put_object(Key=key_name, Body=data)
+    print("Backup has been uploaded successfully.....!!!")
+    print("...")
+
+bucket_name ="pythonfordevops2609"
+region ="us-east-2"
 
 create_bucket(s3)
+backup(source, destination)
+upload_backup(s3, backup_file_name, bucket_name,"my_backup.tar.gz")
 show_bucket(s3)
 
+
+
+
     
+
 
